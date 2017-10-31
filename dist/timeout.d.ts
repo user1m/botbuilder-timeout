f6cc0c87965f7805e1cb975bcce18f5c1958bb8f
Claudius Mbemba <mbembac@gmail.com>
2017-10-31 15:15:48 -0700

    added typescript declaration file

---
+++ dist/timeout.d.ts
@@ -0,0 +1,20 @@
+import * as builder from "botbuilder";
+export interface TimeoutOptions {
+    PROMPT_IF_USER_IS_ACTIVE_MSG: string;
+    PROMPT_IF_USER_IS_ACTIVE_BUTTON_TEXT: string;
+    PROMPT_IF_USER_IS_ACTIVE_TIMEOUT: number;
+    END_CONVERSATION_MSG: string;
+    END_CONVERSATION_TIMEOUT: number;
+}
+export declare class Timeout {
+    private bot;
+    promptHandler: any;
+    endConvoHandler: any;
+    private sessionAlias;
+    private options;
+    constructor(bot: builder.UniversalBot, options: TimeoutOptions);
+    init(): void;
+    private endConversation(session);
+    promptUserIsActive(session: builder.Session): void;
+}
+export declare function setConversationTimeout(bot: builder.UniversalBot, options: TimeoutOptions): void;
